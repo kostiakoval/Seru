11a4ac151bc163ab99d77994e9e90ac5073556a8
Konstantin Koval <konstantin@rocketfarm.no>
2014-08-31 14:48:07 +0200

    insert new object

--- Seru/Source/ManagedObject.swift
+++ Seru/Source/ManagedObject.swift
@@ -7,8 +7,31 @@
 //
 
 import Foundation
+import CoreData
 
-protocol ManagedObjectInfo {
+public protocol ManagedObjectInfo {
   
   class func entityName () -> String
+  class func entity(inContext moc: NSManagedObjectContext) -> NSEntityDescription
+}
+
+
+extension NSManagedObject : ManagedObjectInfo{
+
+  public class func entityName () -> String {
+    let fullName: NSString = NSStringFromClass(self)
+    let dot = fullName.rangeOfString(".", options: NSStringCompareOptions.BackwardsSearch)
+    let className = fullName.substringFromIndex(dot.location + dot.length)
+    return className
+  }
+
+  public class func entity(inContext moc: NSManagedObjectContext) -> NSEntityDescription {
+    let name = entityName()
+    return NSEntityDescription.entityForName(name, inManagedObjectContext: moc)
+  }
+
+  public class func insert(inContext moc: NSManagedObjectContext) -> Self {
+    let object = self(entity: entity(inContext: moc), insertIntoManagedObjectContext: moc)
+    return object
+  }
 }
