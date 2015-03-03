d02bc676f1cd06413ebb864107cb27a7e8b7a9c1
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-03 21:29:48 +0100

    update pod. Add carthage run on travis

--- ExamplePod/Pods/Target Support Files/Pods-Seru/Pods-Seru-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Seru : NSObject
-@end
-@implementation PodsDummy_Pods_Seru
-@end
--- ExamplePod/Pods/Target Support Files/Pods-Seru/Pods-Seru-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SeruVersionNumber;
-FOUNDATION_EXPORT const unsigned char SeruVersionString[];
-
