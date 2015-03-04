144f1a031adf892949bbdff03856a3d90f68c84d
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-04 20:13:01 +0100

    setup seruDemo project

--- Example/SeruDemo/DetailViewController.swift
+++ Example/SeruDemo/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailViewController: UIViewController {
 
--- Example/SeruDemo/MasterViewController.swift
+++ Example/SeruDemo/MasterViewController.swift
@@ -2,84 +2,95 @@
 //  MasterViewController.swift
 //  SeruDemo
 //
-//  Created by Konstantin Koval on 04/03/15.
-//  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
+//  Created by Konstantin Koval on 27/12/14.
+//  Copyright (c) 2014 Konstantin Koval. All rights reserved.
 //
 
 import UIKit
+import CoreData
+import Seru
 
-class MasterViewController: UITableViewController {
-
-    var objects = NSMutableArray()
-
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
+class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+  
+  var seruStack: Seru!
+  var objects: [NSManagedObject] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    navigationItem.leftBarButtonItem = self.editButtonItem()
+    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+    navigationItem.rightBarButtonItem = addButton
+    reloadData()
+  }
+  
+  func reloadData() {
+    
+    seruStack.performInMainContext { context in
+      let fetch = NSFetchRequest(entityName: "Entity")
+      var error: NSError?
+      if let result = context.executeFetchRequest(fetch, error: &error) as? [NSManagedObject] {
+        self.objects = result
+        self.tableView.reloadData()
+      }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
+  }
+  
+  func insertNewObject(sender: AnyObject) {
+    
+    seruStack.performBackgroundSave({ context in
+      var object = NSEntityDescription.insertNewObjectForEntityForName("Entity", inManagedObjectContext: context) as NSManagedObject
+      object.setValue(NSDate(), forKey: "time")
+      
+      },completion: { completed in
+        self.reloadData()
+    })
+  }
+  
+  // MARK: - Table View
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return objects.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+    configureCell(cell, atIndexPath: indexPath)
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
+  
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+      
+      let objectID = (self.objects[indexPath.row] as NSManagedObject).objectID
+      seruStack.performBackgroundSave({ (context: NSManagedObjectContext) in
+        let object = context.objectWithID(objectID)
+        context.deleteObject(object)
+        }, completion: { result in
+          self.reloadData()
+      })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+    let object = objects[indexPath.row]
+    cell.textLabel!.text = object.valueForKey("time")!.description
+  }
+  // MARK: - Segues
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "showDetail" {
+      if let indexPath = self.tableView.indexPathForSelectedRow() {
+        let object = objects[indexPath.row].valueForKey("time") as NSDate
+        (segue.destinationViewController as DetailViewController).detailItem = object
+      }
     }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insertObject(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
-    // MARK: - Segues
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as NSDate
-            (segue.destinationViewController as DetailViewController).detailItem = object
-            }
-        }
-    }
-
-    // MARK: - Table View
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-
-        let object = objects[indexPath.row] as NSDate
-        cell.textLabel!.text = object.description
-        return cell
-    }
-
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeObjectAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
+  }
 
 }
 
